775510af0f513605603e3b910d3560fa945fa37d
Henrik <henrik@heka.no>
2024-11-30 01:34:20 +0000

    Initial commit

---
+++ join-domain.sh
@@ -0,0 +1,17 @@
+echo "- Setting timezone"
+timedatectl set-timezone Europe/Oslo
+
+echo "- Installing packages"
+apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y > /dev/null
+
+echo "- Enabling password login"
+sudo sed -E -i 's/^#?PasswordAuthentication (no|yes)/PasswordAuthentication yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
+
+read "Domain: " AD_DOMAIN
+read "Username: " AD_USERNAME
+
+echo "- Joining domain"
+realm join --membership-software=samba --client-software=sssd --user="$AD_USERNAME" "$AD_DOMAIN"
+
+echo "- Enabling mkhomedir"
+pam-auth-update --enable mkhomedir
